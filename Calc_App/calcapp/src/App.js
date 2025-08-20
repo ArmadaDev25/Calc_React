62f287e2c06627b6185734274829a7d3965c3cef
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2025-08-19 21:13:17 -0700

    Boop

--- Calc_App/calcapp/src/App.js
+++ Calc_App/calcapp/src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [numDisplay, setDisplayNum] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    // Main DIV that will hold all of the Elements for the 
+    <div>
+    <h1>Calulator But REACT</h1>
+    <p>{numDisplay}</p>
+
+    <button>1</button>
+    <button>2</button>
+    <button>3</button>
+    <button>4</button>
+    <button>5</button>
+    <button>6</button>
+    <button>7</button>
+    <button>8</button>
+    <button>0</button>
     </div>
+
   );
 }
 
