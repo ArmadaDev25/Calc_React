a2a407f845a61636f08b879a2d51ef832e5d7e5b
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2025-08-19 23:24:17 -0700

    Update App.js

--- Calc_App/calcapp/src/App.js
+++ Calc_App/calcapp/src/App.js
@@ -4,6 +4,30 @@ import React, { useState } from 'react';
 
 function App() {
   const [numDisplay, setDisplayNum] = useState(0);
+  const [num1, setnum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+  
+  // This is a boolean to check if num1 is set
+  const is = false;
+
+  function Num1 () {
+    setDisplayNum(1);
+  }
+  function enterNum2 () {
+    setDisplayNum(2);
+  }
+  function Num3 () {
+    setDisplayNum(3);
+  }
+    function enterNum4 () {
+    setDisplayNum(4);
+  }
+    function enterNum5 () {
+    setDisplayNum(5);
+  }
+    function enterNum6 () {
+    setDisplayNum(6);
+  }
 
   return (
     
@@ -11,17 +35,28 @@ function App() {
     <div>
     <h1>Calulator But REACT</h1>
     <p>{numDisplay}</p>
+    
+    {/* Div For Buttons 1 - 3 */}
+    <div>
+    <button onClick={Num1}>1</button> 
+    <button onClick={enterNum2}>2</button>
+    <button onClick={Num3}>3</button>
+    </div>
 
-    <button>1</button>
-    <button>2</button>
-    <button>3</button>
-    <button>4</button>
-    <button>5</button>
-    <button>6</button>
+    <div>
+    <button onClick={enterNum2}>4</button>
+    <button onClick={enterNum2}>5</button>
+    <button onClick={enterNum2}>6</button>
+    </div>
+
+    <div>
     <button>7</button>
     <button>8</button>
-    <button>0</button>
+    <button>9</button>
     </div>
+
+    </div>
+    
 
   );
 }
